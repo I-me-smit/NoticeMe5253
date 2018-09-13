9d60f078737bcdc98e9428d210fc40022b8c762b
Smit Limbani <smitrl77@gmail.com>
2018-09-13 08:27:59 -0700

    navu

---
+++ app/src/main/java/com/example/smitlimbani/noticeme5253/NoticeDetails.java
@@ -0,0 +1,58 @@
+package com.example.smitlimbani.noticeme5253;
+
+public class NoticeDetails
+{
+    private String notice_sender;
+
+    private String notice_title;
+
+    private String notice_content;
+
+    private String notice_expiry;
+
+    public String getNotice_sender ()
+    {
+        return notice_sender;
+    }
+
+    public void setNotice_sender (String notice_sender)
+    {
+        this.notice_sender = notice_sender;
+    }
+
+    public String getNotice_title ()
+    {
+        return notice_title;
+    }
+
+    public void setNotice_title (String notice_title)
+    {
+        this.notice_title = notice_title;
+    }
+
+    public String getNotice_content ()
+    {
+        return notice_content;
+    }
+
+    public void setNotice_content (String notice_content)
+    {
+        this.notice_content = notice_content;
+    }
+
+    public String getNotice_expiry ()
+    {
+        return notice_expiry;
+    }
+
+    public void setNotice_expiry (String notice_expiry)
+    {
+        this.notice_expiry = notice_expiry;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [notice_sender = "+notice_sender+", notice_title = "+notice_title+", notice_content = "+notice_content+", notice_expiry = "+notice_expiry+"]";
+    }
+}
